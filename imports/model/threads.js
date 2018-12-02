249b612bd3834ed946ab718d6206f9ece98ad00e
Will <lian.wang01@net263.com>
2018-12-02 17:43:29 -0500

    add models

---
+++ imports/model/threads.js
@@ -0,0 +1,43 @@
+// - category: Email
+// - subject
+Threads = new Mongo.Collection('threads');
+
+// - email
+// - name
+Contacts = new Mongo.Collection('contacts');
+
+// - threadId
+// - userType: user, contact
+// - userId
+ThreadUsers = new Mongo.Collection('thread_users');
+
+// - threadId
+// - userType: user, contact
+// - userId
+// - content
+Messages = new Mongo.Collection('messages');
+
+WeWork = {};
+WeWork.createThread = (subject) => {
+  return Threads.insert({
+    subject
+  });
+};
+
+Threads.helpers({
+  addMember(userType, userId) {
+    return ThreadUsers.insert({
+      threadId: this._id,
+      userType,
+      userId
+    });
+  },
+  addMessage(userType, userId, message) {
+    return Messages.insert({
+      threadId: this._id,
+      userType,
+      userId,
+      content: message.content
+    });
+  }
+});
