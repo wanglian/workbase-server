fa095f0ae279892583e2c78bb2b9a39d61e2a9b0
Will <lian.wang01@net263.com>
2018-12-08 11:04:31 -0500

    fix menu

--- imports/ui/inbox/inbox.js
+++ imports/ui/inbox/inbox.js
@@ -1,5 +1,12 @@
 import './inbox.html';
 
 Template.InboxMenu.events({
-
+  "click #btn-add-channel"(e, t) {
+    e.preventDefault();
+    Modal.show('AddChannelModal');
+  },
+  "click #btn-add-roster"(e, t) {
+    e.preventDefault();
+    Modal.show('AddRosterModal');
+  }
 });
