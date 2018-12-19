9aa4cb019a6d14a58bbd201c371c2b301d2fc9fa
Will <lian.wang01@net263.com>
2018-12-19 12:59:12 -0500

    show file icon

--- imports/startup/client/helper.js
+++ imports/startup/client/helper.js
@@ -22,8 +22,53 @@ Template.registerHelper('inRouter', function(router) {
   return Router.current().route.getName() === router;
 });
 
-Template.registerHelper('filetype', function(ext) {
-  return "<i class='fa fa-paperclip'></i>";
+const filetype = (type) => {
+  if (type.match(/image/)) {
+    return "image";
+  } else if (type.match(/pdf/)) {
+    return "pdf";
+  } else if (type.match(/audio/)) {
+    return "audio";
+  } else if (type.match(/video/)) {
+    return "video";
+  } else if (type.match(/word/)) {
+    return "word";
+  } else if (type.match(/spreadsheet/)) {
+    return "excel";
+  } else if (type.match(/ppt/)) {
+    return "powerpoint";
+  } else if (type.match(/zip/)) {
+    return "archive";
+  } else if (type.match(/text|json/)) {
+    return "text";
+  } else {
+    return 'file';
+  }
+};
+
+Template.registerHelper('fileicon', function(type) {
+  switch (filetype(type)) {
+  case 'image':
+    return "<i class='fa fa-file-image-o text-info'></i>";
+  case 'pdf':
+    return "<i class='fa fa-file-pdf-o text-danger'></i>";
+  case 'audio':
+    return "<i class='fa fa-file-audio-o text-info'></i>";
+  case 'video':
+    return "<i class='fa fa-file-video-o text-info'></i>";
+  case 'word':
+    return "<i class='fa fa-file-word-o text-blue'></i>";
+  case 'excel':
+    return "<i class='fa fa-file-excel-o text-success'></i>";
+  case 'powerpoint':
+    return "<i class='fa fa-file-powerpoint-o text-info'></i>";
+  case 'archive':
+    return "<i class='fa fa-file-archive-o text-info'></i>";
+  case 'text':
+    return "<i class='fa fa-file-text-o text-info'></i>";
+  default:
+    return "<i class='fa fa-file-o'></i>";
+  }
 });
 
 let moment = require('moment');
