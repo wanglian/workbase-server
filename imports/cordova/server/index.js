dbde27bcfa4087fb4948ce3f81ad3656b6b7bab5
Will <lian.wang01@net263.com>
2018-12-23 14:17:03 -0500

    enable sound

--- imports/cordova/server/index.js
+++ imports/cordova/server/index.js
@@ -13,11 +13,11 @@ Meteor.startup(function() {
     // gcm: {
     //   apiKey: 'xxxxxxx',  // GCM/FCM server key
     // }
-    // production: true,
-    // 'sound' true,
-    // 'badge' true,
-    // 'alert' true,
-    // 'vibrate' true,
+    production: true,
+    sound: true,
+    badge: true,
+    alert: true,
+    vibrate: true
     // 'sendInterval': 15000, Configurable interval between sending
     // 'sendBatchSize': 1, Configurable number of notifications to send per batch
     // 'keepNotifications': false,
@@ -39,7 +39,7 @@ Messages.after.insert(function(userId, doc) {
       title: user.internalName() + ': ' + thread.subject,
       text:  message.summary,
       badge: 1,
-      // sound: fileInPublicFolder
+      sound: 'default',
       query: {
         userId: {$in: userIds}
       }
