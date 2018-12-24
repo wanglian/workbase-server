5e9e017ad05c97b4ee0f751af18af1b405a6d3e5
Will <lian.wang01@net263.com>
2018-12-24 10:10:02 -0500

    add mongodb indexes

--- imports/model/server/index.js
+++ imports/model/server/index.js
@@ -3,3 +3,12 @@ import './hooks';
 import './functions';
 import './methods';
 import './publications';
+
+Meteor.startup(function() {
+  ThreadUsers._ensureIndex({category: 1});
+  ThreadUsers._ensureIndex({threadId: 1});
+  ThreadUsers._ensureIndex({userType: 1});
+  ThreadUsers._ensureIndex({userId: 1});
+  ThreadUsers._ensureIndex({read: 1});
+  Messages._ensureIndex({threadId: 1});
+});
