55a232960a34d9863c69ecf92adb2f4ea60dc2b7
Lian Wang <wanglian1024@gmail.com>
2019-02-26 12:30:17 -0500

    paginate rosters

--- imports/roster/client/index.js
+++ imports/roster/client/index.js
@@ -5,8 +5,12 @@ import './style.css';
 
 RosterController = ApplicationController.extend({
   template: 'Roster',
+  perPage: 25,
   subscriptions() {
     // this.subscribe("roster");
+  },
+  limit: function() {
+    return parseInt(this.params.query.limit) || this.perPage;
   },
   userId() {
     return this.params._id;
@@ -17,12 +21,19 @@ RosterController = ApplicationController.extend({
   data() {
     let user = Users.findOne(this.userId());
     let type = this.type() === 'external' ? 'Contacts' : 'Users';
+
+    let query = _.clone(this.params.query);
+    _.extend(query, {limit: this.limit() + this.perPage});
+    let nextPath = this.route.path(this.params, {query});
+    let hasMore = Users.find({"profile.type": type}).count() > this.limit();
+
     return {
       type,
-      users: Users.find({"profile.type": type}, {sort: {"profile.name": 1}}),
+      users: Users.find({"profile.type": type}, {sort: {"profile.name": 1}, limit: this.limit()}),
       user,
       hasRight: !!user,
-      hasSidebar: false
+      hasSidebar: false,
+      nextPath: hasMore ? nextPath : null
     };
   }
 });
