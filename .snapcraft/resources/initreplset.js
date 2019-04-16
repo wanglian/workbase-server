3c81b537f5721474da8842e3e31e0cf2e5f9a13a
Lian Wang <wanglian1024@gmail.com>
2019-04-16 14:04:03 -0400

    add snap config files

---
+++ .snapcraft/resources/initreplset.js
@@ -0,0 +1,12 @@
+var ism = db.isMaster();
+if (!ism.ismaster) {
+  rs.initiate({
+    _id: 'rs0',
+    members: [
+      {
+        _id: 0,
+        host: 'localhost:27017'
+      }
+    ]
+  });
+}
