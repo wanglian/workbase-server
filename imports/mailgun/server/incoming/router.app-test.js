41e634e7805475011c01dffca393d50881a739a7
Lian Wang <wanglian1024@gmail.com>
2019-04-20 21:49:12 -0400

    refactor mailgun router test

---
+++ imports/mailgun/server/incoming/router.app-test.js
@@ -0,0 +1,9 @@
+import '/imports/test/test-helpers';
+
+describe('mailgun api', function() {
+  it('should respond to post: /api/v1/mailgun', function() {
+    chai.request("http://localhost:3000").post('/api/v1/mailgun').then(function(res) {
+      expect(res).to.have.status(200);
+    });
+  })
+})
--- imports/test/test-helpers.js
+++ imports/test/test-helpers.js
@@ -1,10 +1,12 @@
 import './factories';
 
-import chai from 'chai';
 import lodash from 'lodash';
 
 faker = require('faker');
 sinon = require('sinon');
+chai  = require('chai');
+chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 expect = chai.expect;
 should = chai.should();
 _ = lodash;
