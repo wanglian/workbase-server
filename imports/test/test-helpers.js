573451f58c8e26e26cb5b87a65f4e5225f956b9d
Lian Wang <wanglian1024@gmail.com>
2019-04-23 10:14:23 -0400

    move denodeify to /lib

--- imports/test/test-helpers.js
+++ imports/test/test-helpers.js
@@ -10,14 +10,3 @@ chai.use(chaiHttp);
 expect = chai.expect;
 should = chai.should();
 _ = lodash;
-
-// Convert an NPM-style function returning a callback to one that returns a Promise.
-denodeify = f => (...args) => new Promise((resolve, reject) => {
-  f(...args, (err, val) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(val);
-    }
-  });
-});
