1f9edbe1fbdd88acb20a149969421a1a753c690b
Lian Wang <wanglian1024@gmail.com>
2019-04-29 18:28:57 -0400

    improve css

--- imports/admin/server/cron-jobs.js
+++ imports/admin/server/cron-jobs.js
@@ -2,11 +2,11 @@ import moment from 'moment';
 
 SyncedCron.add({
   name: "Crunch some important numbers for the marketing department",
-  schedule: function(parser) {
+  schedule(parser) {
     // parser is a later.parse object
     return parser.text("5 0 * * ? *");
   },
-  job: function() {
+  job() {
     let d = moment().subtract(1, "days");
     MessageDailyRecords.stat(d.year(), d.month() + 1, d.date());
   }
