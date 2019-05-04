6539657cc2b0208f4eb539cf2ec8a808593f8108
Lian Wang <wanglian1024@gmail.com>
2019-05-03 21:51:00 -0400

    fix lint

--- imports/files/client/helper.js
+++ imports/files/client/helper.js
@@ -1,27 +1,27 @@
 const filetype = (type) => {
+  let _t = "file";
   if (type.match(/image/)) {
-    return "image";
+    _t = "image";
   } else if (type.match(/pdf/)) {
-    return "pdf";
+    _t = "pdf";
   } else if (type.match(/opendocument/)) {
-    return "odf";
+    _t = "odf";
   } else if (type.match(/audio/)) {
-    return "audio";
+    _t = "audio";
   } else if (type.match(/video/)) {
-    return "video";
+    _t = "video";
   } else if (type.match(/word/)) {
-    return "word";
+    _t = "word";
   } else if (type.match(/spreadsheet/)) {
-    return "excel";
+    _t = "excel";
   } else if (type.match(/powerpoint/)) {
-    return "ppt";
+    _t = "ppt";
   } else if (type.match(/zip/)) {
-    return "archive";
+    _t = "archive";
   } else if (type.match(/text|json/)) {
-    return "text";
-  } else {
-    return 'file';
+    _t = "text";
   }
+  return _t;
 };
 Template.registerHelper('filetype', filetype);
 
